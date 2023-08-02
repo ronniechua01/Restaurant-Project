8fd2aa994501768d72e5b73a258a406611844aeb
Stephen <sjpchua@iskolarngbayan.pup.edu.ph>
2023-08-03 05:17:12 +0800

    added error for not connecting to the database

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,38 @@
-import app from "./server.js"
-import mongodb from "mongodb"
-import dotenv from "dotenv"
 
-//allows to access env variables
-dotenv.config()
+// Import the express app from server.js
+import app from "./server.js";
 
-//access to mongo client 
-const MongoClient = mongodb.MongoClient
+// Import necessary packages
+import mongodb from "mongodb";
+import dotenv from "dotenv";
 
-//create port number (if port cannot be access, use port:8000)
-const port = process.env.PORT || 8000
+// Load environment variables
+dotenv.config();
 
-//connect to database
-MongoClient.connect(
-    //pass information
-    process.env.RESTREVIEWS_DB_URI,
-    {
-        //pass options for accessing db
-        //only 50 can connect at a time
-        maxPoolSize: 50,
-        setTimeout: 2500,
-        useNewURLParse: true 
-    }
-    //catch error
-    .catch (err => {
-        //write the error in the console and exit the process
-        console.error(err.stack)
-        process.exit(1)
+// Access to the MongoDB client
+const MongoClient = mongodb.MongoClient;
+
+// Define the port number (fallback to port 8000 if not specified in environment variables)
+const port = process.env.PORT || 8000;
+
+// Connect to the database and start the server
+
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+    poolSize: 50,
+    wtimeout: 2500,
+    useNewUrlParser: true,
+  })
+    .then((client) => {
+      // The database connection is successful
+      console.log("Connected to the database!");
+      // Start the server once the database connection is established
+      app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+      });
     })
-    .then (async client => {
-        //start the webserver
-        app.listen(port, () => {
-            console.log('listening on port ${port}')
-        })
-    })
-)
+    .catch((err) => {
+      // If there's an error during the database connection, log the error and exit the process
+      console.error("Error connecting to the database:", err);
+      process.exit(1);
+    });
+  
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 //import area
 
-import Express from "express";
+import express from "express";
 import cors from "cors";
-import { restaurants } from "./api/restaurants.route.js";
+import restaurants from "./api/restaurants.route.js";
 
 //create express server
 const app = express()
